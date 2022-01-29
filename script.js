e684aeb112342362e5e9e3ee03e4a7d086087e2a
black_boot.exe <77060124+mahdixabid@users.noreply.github.com>
2022-01-30 00:47:04 +0100

    get total

---
+++ script.js
@@ -0,0 +1,35 @@
+// Objects :
+// get total
+// create product
+// save localstorage
+// clear inputs
+// read 
+// count
+// delete
+// update
+// search
+// clean data
+
+
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+
+// get total
+function getTotal() {
+    if (price.value != '') {
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+        total.innerHTML = result;
+        total.style.background = '#040';
+    } else {
+        total.innerHTML = '';
+        total.style.background = 'red';
+    }
+}
